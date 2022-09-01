80b82c0016e13a97530e99be307d9f716a7948b8
Matias Flores <arnod7991@gmail.com>
2022-09-01 16:55:32 -0400

    add table, export report delivery detail

--- src/app/modules/main/report/configs/table-columns.ts
+++ src/app/modules/main/report/configs/table-columns.ts
@@ -1,5 +1,6 @@
 import { formatDate, formatNumber } from '@angular/common';
 import { TableColumns } from 'src/app/components/data-table/data-table.interface';
+import { DeliveryDetail } from '../interface/delivery-detail.interface';
 import { PaymentDetail } from '../interface/payment-detail.interface';
 
 export const paymentDetailColumns: TableColumns<PaymentDetail> = [
@@ -48,5 +49,44 @@ export const paymentDetailColumns: TableColumns<PaymentDetail> = [
     headerName: 'Fecha Pago',
     valueFormatter: ({ fecha_pago }) =>
       fecha_pago ? formatDate(fecha_pago, 'dd/MM/yyyy', 'es') : '--',
+  },
+];
+
+export const deliveryDetailColumns: TableColumns<DeliveryDetail> = [
+  {
+    field: 'nombre',
+    headerName: 'Nombre',
+  },
+  {
+    field: 'ciudad',
+    headerName: 'Ciudad',
+  },
+  {
+    field: 'nombre_cliente',
+    headerName: 'Cliente',
+  },
+  {
+    field: 'telefono_cliente',
+    headerName: 'Telefono',
+  },
+  {
+    field: 'monto',
+    headerName: 'Monto',
+    valueFormatter: ({ monto }) => formatNumber(Number(monto), 'es', '.2-2'),
+  },
+  {
+    field: 'comision',
+    headerName: 'Comision',
+    valueFormatter: ({ comision }) =>
+      formatNumber(Number(comision), 'es', '.2-2'),
+  },
+  {
+    field: 'metodo_pago',
+    headerName: 'Metodo Pago',
+  },
+  {
+    field: 'fecha',
+    headerName: 'Fecha',
+    valueFormatter: ({ fecha }) => formatDate(fecha, 'dd/MM/yyyy', 'es'),
   },
 ];
--- src/app/modules/main/report/pages/delivery-detail/delivery-detail.component.ts
+++ src/app/modules/main/report/pages/delivery-detail/delivery-detail.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { buildform } from 'src/app/components/text-field/text-field.util';
 import { DateUtils } from 'src/app/utils/date.util';
+import { ExcelUtils } from 'src/app/utils/excel.util';
 import { handleRequest } from 'src/app/utils/handle-request';
 import { deliveryDetailSchema } from '../../configs/form-schema';
+import { deliveryDetailColumns } from '../../configs/table-columns';
+import { DeliveryDetail } from '../../interface/delivery-detail.interface';
 import { ReportService } from '../../services/report.service';
 
 @Component({
@@ -12,7 +15,9 @@ import { ReportService } from '../../services/report.service';
 })
 export class DeliveryDetailComponent implements OnInit {
   deliveryDetailSchema = deliveryDetailSchema;
+  deliveryDetailColumns = deliveryDetailColumns;
   form = buildform(deliveryDetailSchema);
+  deliveryDetailList?: DeliveryDetail[];
 
   constructor(private reportService: ReportService) {}
 
@@ -21,18 +26,22 @@ export class DeliveryDetailComponent implements OnInit {
   }
 
   async getDeliveryDetail() {
-    console.log('init res');
     const value = this.form.value;
+    this.deliveryDetailList = undefined;
     const res = await handleRequest(() =>
       this.reportService.getDeliveryDetail({
         ...value,
         end: DateUtils.getMaxHour(value.end),
       })
     );
-    if (res) console.log('res', res);
+    if (res) this.deliveryDetailList = res.data;
   }
 
   filter() {
     this.getDeliveryDetail();
+  }
+
+  downloadExcel(){
+    ExcelUtils.download(this.deliveryDetailList!, 'delivery detail');
   }
 }
