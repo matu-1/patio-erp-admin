efa1199d6035822371d6f874b768141f780e0d9a
Matias Manuel Flores <arnod7991@gmail.com>
2022-10-03 21:29:36 -0400

    add tables payment qr

--- src/app/modules/main/payment-qr/pages/list/list.component.ts
+++ src/app/modules/main/payment-qr/pages/list/list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { buildform } from 'src/app/components/text-field/text-field.util';
 import { ObjectUtils } from 'src/app/utils/object.util';
 import { filterPaymentQRSchema } from '../../configs/form-schema';
+import { PaymentQR } from '../../interfaces/payment-qr.interface';
+import { paymentQrColumns } from '../../configs/table-columns';
 
 @Component({
   selector: 'app-list',
@@ -12,6 +14,8 @@ export class ListComponent implements OnInit {
   title = 'Pagos QR';
   form = buildform(filterPaymentQRSchema);
   filterPaymentQRSchema = filterPaymentQRSchema;
+  paymentQrColumns = paymentQrColumns;
+  paymentsQR?: PaymentQR[];
 
   constructor() {}
 
