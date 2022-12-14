157687017b291b6a2e1e37a71ab137c16d57acf6
Matias Flores R <arnod7991@gmail.com>
2022-12-14 09:46:59 -0400

    update api urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://erp-patio.herokuapp.com/api',
-  patioStoreApiUrl: 'https://patio-driver.herokuapp.com/api',
+  apiUrl: 'https://erp-patio.azurewebsites.net/api',
+  patioStoreApiUrl: 'https://patio-driver.azurewebsites.net/api',
   patioStoreToken:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAsInVzZXJuYW1lIjoibWF0dUBnbWFpbC5jb20iLCJpYXQiOjE2NjIzOTM4NzUsImV4cCI6MTY5MzkyOTg3NX0.arMjehZiz8M5Eo0q_LpfjSU_SR2d8Y7w0gO7NNgFexI',
 };
