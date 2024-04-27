cf7d76daa204a32a100ef3e600ebb1d43f2f688b
Matias Manuel Flores <arnod7991@gmail.com>
2024-04-27 10:51:28 -0400

    update enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://erp-patio.azurewebsites.net/api',
-  patioStoreApiUrl: 'https://patio-driver.azurewebsites.net/api',
+  apiUrl: 'https://www.erp-patio.patiodelivery2.com/api',
+  patioStoreApiUrl: 'https://www.patio-driver.patiodelivery2.com/api',
   patioStoreToken:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAsInVzZXJuYW1lIjoibWF0dUBnbWFpbC5jb20iLCJpYXQiOjE2OTI3OTk2NjIsImV4cCI6MTcyNDMzNTY2Mn0.LCD5N569v9lsqXHC4b6We_5JXCra-a0OioxsAeR0ync',
 };
